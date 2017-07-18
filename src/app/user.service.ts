240b692bc40be183d733733a862f181116caec33
Donat Travin <donattravin@gmail.com>
2017-07-18 15:40:06 +0300

    fixed user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -15,27 +15,29 @@ export class UserService {
 
   public setCurrentUser(user: UserModel) {
     this.demoShopHttpService.get(`/users?login=${user.login}`)
-      .map((response: Response) => response.text())
+      .map((response: Response) => {
+        return response.text()
+      })
       .map((jsonUser: string) => JSON.parse(jsonUser))
-      .do(user => {
+      .do(([user]) => {
         this.currentUser = new UserModel(
           user.login,
           user.password,
-          user.userId,
-          user.userRoleId
+          user.id,
+          user.roleId
         );
-
-        return user.userRoleId;
       })
-      .do((userRoleId: number) => {
-        return this.demoShopHttpService.get(`/roles?id=${userRoleId}`);
+      .mergeMap(([user]) => {
+        return this.demoShopHttpService.get(`/roles?id=${user.roleId}`)
       })
-      .map(response => {
-        return response.text()
-      })
+      .map(response => response.text())
       .map(jsonRole => JSON.parse(jsonRole))
-      .subscribe(role => {
+      .subscribe(([role]) => {
         this.currentUser.isAdministrator = role['name'] === 'Admin';
       })
+  }
+
+  public isUserAdmin() {
+    return this.currentUser.isAdministrator;
   }
 }
