47348541c1222e3abfa31a47203968e83af7e0fd
Donat Travin <dtravin@griddynamics.com>
2018-03-15 16:59:42 -0700

    Fixed styled issue

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -32,7 +32,7 @@ export class ProductFormComponent implements OnInit {
   ngOnInit() {
     this.productForm = this._formBuilder.group({
       name: [this.product.name, Validators.required],
-      linkToImage: [this.product.image, Validators.compose([Validators.required]), imageValidator],
+      linkToImage: [this.product.image, Validators.required, imageValidator],
       price: [this.product.cost, Validators.compose([Validators.required, Validators.pattern(/^\d*(\.\d{1,2})?$/)])],
       rating: [this.product.rating, Validators.required],
       gender: [Gender[this.product.gender], Validators.required],
